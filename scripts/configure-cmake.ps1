4b2887a3064f896a73525a1d8ab887049e0d6f12
George Wu <uiharu@buaa.edu.cn>
2018-10-23 21:11:50 +0200

    Initialize CMake for AppVeyor

---
+++ scripts/configure-cmake.ps1
@@ -0,0 +1,42 @@
+[string]$arch = [string]::Empty;
+
+if ($Env:ARCH -eq "Win64") {
+    $arch = "Win64";
+    echo "Arch: $arch";
+    # prepare for generator concatenation
+    $arch = " $arch";
+} else {
+    echo 'Arch: (default -> Win32)';
+}
+
+[string]$workerImage = $Env:APPVEYOR_BUILD_WORKER_IMAGE;
+
+echo "Build image: $workerImage";
+
+[string]$generator = [string]::Empty;
+
+if ($workerImage -eq 'Visual Studio 2017') {
+    $generator = "Visual Studio 15 2017$arch";
+} elseif ($workerImage -eq 'Visual Studio 2015') {
+    $generator = "Visual Studio 14 2015$arch";
+} else {
+    echo 'Error: Unsupported worker image.';
+    $generator = "Visual Studio 15 2017$arch";
+    exit 1;
+}
+
+echo '------******------';
+
+if (![System.IO.Directory]::Exists('build\vc')) {
+    mkdir build\vc
+}
+
+cd build\vc
+
+cmake --version
+
+[string]$cmakeGenCmd = "cmake ..\.. -G `"$generator`""
+
+[ScriptBlock]$script = [ScriptBlock]::Create($cmakeGenCmd);
+
+Invoke-Command -ScriptBlock $script
