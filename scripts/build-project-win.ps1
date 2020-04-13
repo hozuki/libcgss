d13fa81abf910ef48306f4f74043c3550bad81e0
George Wu <uiharu@buaa.edu.cn>
2020-04-13 15:27:11 +0200

    Enable parallel build on Windows

--- scripts/build-project-win.ps1
+++ scripts/build-project-win.ps1
@@ -1,1 +1,2 @@
-& msbuild build\vc\cgss.sln /p:Configuration="$env:CONFIGURATION"
+[Int32]$processorCount = [Environment]::ProcessorCount;
+& msbuild build\vc\cgss.sln /p:Configuration="$env:CONFIGURATION" -maxcpucount:"$processorCount"
