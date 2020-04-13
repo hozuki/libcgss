8c342bb5980249c3570f21f5c744f41bda8845df
George Wu <uiharu@buaa.edu.cn>
2020-04-13 12:34:20 +0200

    Fixed version comparison in UNIX build script

--- build.sh
+++ build.sh
@@ -4,26 +4,35 @@ CLR_RED="\033[0;31m"
 CLR_YELLOW="\033[1;33m"
 CLR_DEF="\033[0m"
 
-command -v cmake >/dev/null 2>&1 || { echo -e >&2 "${CLR_RED}CMake is not found. Aborting.${CLR_DEF}"; return 1; }
+command -v cmake >/dev/null 2>&1 || {
+  echo -e "${CLR_RED}CMake is not found. Aborting.${CLR_DEF}" >&2
+  return 1
+}
 
-CMAKE_VERSION=$(cmake --version | grep -Eo "cmake version ([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)" | cut -d\  -f3)
+CMAKE_VERSION_FOUND=$(cmake --version | grep -Eo "cmake version ([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)" | cut -d\  -f3)
+CMAKE_VERSION_MINIMUM=3.2
 
-if [[ ${CMAKE_VERSION} < "3.2" ]]; then
-    echo -e "${CLR_RED}Unmet requirement: CMake version${CLR_DEF}\n  required: >=3.2\n  found: ${CLR_YELLOW}${CMAKE_VERSION}${CLR_DEF}";
-    return 1;
+# https://stackoverflow.com/a/4024263
+verlte() {
+  [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
+}
+
+if [[ $(verlte "$CMAKE_VERSION_MINIMUM" "$CMAKE_VERSION_FOUND") ]]; then
+  echo -e "${CLR_RED}Unmet requirement: CMake version${CLR_DEF}\n  required: >=${CMAKE_VERSION_MINIMUM}\n  found: ${CLR_YELLOW}${CMAKE_VERSION_FOUND}${CLR_DEF}"
+  exit 1
 fi
 
 if [ ! -d "build" ]; then
-    mkdir build
+  mkdir build
 fi
-cd build
+cd build || exit 2
 if [ ! -d "make" ]; then
-    mkdir make
+  mkdir make
 fi
-cd make
-rm -rf *
+cd make || exit 2
+rm -rf ./*
 cmake -DCMAKE_BUILD_TYPE=MinSizeRel ../..
 make
 cd ../..
 echo Done.
-return 0
+exit 0
