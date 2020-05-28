621a68738940773f0fa76a2183b4b50ce6fbe3a6
George Wu <uiharu@buaa.edu.cn>
2020-05-28 16:45:58 +0200

    Fix CI

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,5 +1,11 @@
 & git submodule update --init --recursive
 
-[String]$extScript = [System.IO.Path]::Combine($PSScriptRoot, "..", "lib", "vgaudio-cpp", "scripts", "ci-install.ps1")
+[String]$vgaudioCppProjectRoot = [System.IO.Path]::Combine($PSScriptRoot, "..", "lib", "vgaudio-cpp")
+[String]$extScriptRoot = [System.IO.Path]::Combine($vgaudioCppProjectRoot, "scripts")
+[String]$extScript = [System.IO.Path]::Combine($extScriptRoot, "ci-install.ps1")
+
+Push-Location $vgaudioCppProjectRoot
 
 . $extScript
+
+Pop-Location
