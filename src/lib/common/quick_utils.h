08052dae9a181c48465068f10a71779f4a9985a3
George Wu <uiharu@buaa.edu.cn>
2020-06-10 09:09:30 +0200

    bswap

--- src/lib/common/quick_utils.h
+++ src/lib/common/quick_utils.h
@@ -15,11 +15,17 @@ CGSS_NS_BEGIN
     }
 
     inline uint16_t bswap(uint16_t v) {
+#if defined(__GNUC__)
+        return __builtin_bswap16(v);
+#elif defined(_MSC_VER)
+        return _byteswap_ushort(v);
+#else
         uint16_t r = (uint16_t)(v & 0xffu);
         r <<= 8;
         v >>= 8;
         r |= v & 0xffu;
         return r;
+#endif
     }
 
     inline int16_t bswap(int16_t v) {
@@ -29,6 +35,11 @@ CGSS_NS_BEGIN
     }
 
     inline uint32_t bswap(uint32_t v) {
+#if defined(__GNUC__)
+        return __builtin_bswap32(v);
+#elif defined(_MSC_VER)
+        return _byteswap_ulong(v);
+#else
         uint32_t r = v & 0xffu;
 
         for (auto i = 0; i < 3; i += 1) {
@@ -38,6 +49,7 @@ CGSS_NS_BEGIN
         }
 
         return r;
+#endif
     }
 
     inline int32_t bswap(int32_t v) {
@@ -47,6 +59,11 @@ CGSS_NS_BEGIN
     }
 
     inline uint64_t bswap(uint64_t v) {
+#if defined(__GNUC__)
+        return __builtin_bswap64(v);
+#elif defined(_MSC_VER)
+        return _byteswap_uint64(v);
+#else
         uint64_t r = v & 0xffu;
 
         for (auto i = 0; i < 7; i += 1) {
@@ -56,6 +73,7 @@ CGSS_NS_BEGIN
         }
 
         return r;
+#endif
     }
 
     inline int64_t bswap(int64_t v) {
@@ -69,10 +87,14 @@ CGSS_NS_BEGIN
         return *(float *)&i;
     }
 
+    inline double bswap(double v) {
+        uint64_t i = bswap(*(uint64_t *)&v);
+        return *(double *)&i;
+    }
+
     inline uint32_t ceil2(uint32_t a, uint32_t b) {
         return (b > 0) ? (a / b + ((a % b) ? 1 : 0)) : 0;
     }
-
 
 
 CGSS_NS_END
